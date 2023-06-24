65e05ec8c685847680616ae892049a609941ab43
chait-Reese <137468704+chait-Reese@users.noreply.github.com>
2023-06-24 13:27:57 +0800

    敵人消失跟特效完成

--- Assets/mind/Bullet.cs
+++ Assets/mind/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject enemyExplosion; // 敵人被銷毀時播放的特效
     void Start()
     {
         Destroy(gameObject, 10); // 子彈預設十秒後會自動刪除自己
@@ -13,6 +14,11 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Target")
+        {
+            // 播放特效
+            Instantiate(enemyExplosion, collision.gameObject.transform.position, Quaternion.identity);
+            Destroy(collision.gameObject); // 碰撞到敵人時，銷毀敵人物件
             Destroy(gameObject);
+        }
     }
 }
